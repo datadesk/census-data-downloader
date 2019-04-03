f884a877350e6b436638f616c73e3475040f3a0e
Peter Andringa <peter.j.andringa@gmail.com>
2019-04-03 17:43:44 -0400

    Add register_downloader decorator

--- census_data_downloader/employment.py
+++ census_data_downloader/employment.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*
 import collections
 from .base import BaseDownloader
+from .registry import register_downloader
 
 
+@register_downloader
 class EmploymentDownloader(BaseDownloader):
     YEAR_LIST = (
         2017,
