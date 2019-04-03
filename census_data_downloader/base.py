11bc167a3958f2bc70f471f4c51d982d02a885e9
palewire <ben.welsh@gmail.com>
2019-04-02 18:46:49 -0700

    A little tidying

--- census_data_downloader/base.py
+++ census_data_downloader/base.py
@@ -166,12 +166,12 @@ class BaseDownloader(object):
         geoid_function = lambda row: row['state'] + row['county'] + row['tract']
         self._download_tables(api_filter, csv_suffix, geoid_function)
 
-    def download_legislative_districts_upper(self, state):
+    def download_state_legislative_districts_upper(self, state):
         """
         Download data for all Census upper legislative districts in the provided state.
         """
         state_obj = getattr(states, state.upper())
-        csv_suffix = f'legislativedistricts_upper_{state_obj.abbr.lower()}'
+        csv_suffix = f'statelegislativedistrictsupper_{state_obj.abbr.lower()}'
         api_filter = {
             'for': 'state legislative district (upper chamber):*',
             'in': f'state: {state_obj.fips}'
@@ -179,12 +179,12 @@ class BaseDownloader(object):
         geoid_function = lambda row: row['state'] + row['state legislative district (upper chamber)']
         self._download_tables(api_filter, csv_suffix, geoid_function)
 
-    def download_legislative_districts_lower(self, state):
+    def download_state_legislative_districts_lower(self, state):
         """
         Download data for all Census lower legislative districts in the provided state.
         """
         state_obj = getattr(states, state.upper())
-        csv_suffix = f'legislativedistricts_lower_{state_obj.abbr.lower()}'
+        csv_suffix = f'statelegislativedistrictslower_{state_obj.abbr.lower()}'
         api_filter = {
             'for': 'state legislative district (lower chamber):*',
             'in': f'state: {state_obj.fips}'
@@ -209,8 +209,8 @@ class BaseDownloader(object):
         self.download_usa()
         for state in states.STATES:
             self.download_tracts(state.abbr)
-            self.download_legislative_districts_upper(state.abbr)
-            self.download_legislative_districts_lower(state.abbr)
+            self.download_state_legislative_districts_upper(state.abbr)
+            self.download_state_legislative_districts_lower(state.abbr)
 
     def _download_tables(self, api_filter, csv_suffix, geoid_function):
         """
