06c514a08f67b30549678d678beaf5a70a2d99e0
Peter Andringa <peter.j.andringa@gmail.com>
2019-04-03 17:42:29 -0400

    Add register_downloader decorator

--- census_data_downloader/language.py
+++ census_data_downloader/language.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*
 import collections
 from .base import BaseDownloader
+from .registry import register_downloader
 
 
+@register_downloader
 class LanguageDownloader(BaseDownloader):
     PROCESSED_TABLE_NAME = 'language'
     RAW_TABLE_NAME = 'B16001'
