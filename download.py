75b4f707e144e9af3b029244f2c01477c1908c7f
palewire <ben.welsh@gmail.com>
2019-05-06 17:39:02 -0700

    Redownloaded a bunch of stuff

--- download.py
+++ download.py
@@ -15,7 +15,7 @@ def main():
     ch.setFormatter(formatter)
     logger.addHandler(ch)
     # Download em all
-    census_data_downloader.download_usa(data_dir="./data")
+    census_data_downloader.download_usa(data_dir="./data", force=True)
 
 
 if __name__ == '__main__':
