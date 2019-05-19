b21c83c069ec747326aeb0fae6ffa9c077b41ee7
Peter Andringa <peter.j.andringa@gmail.com>
2019-05-19 16:05:53 -0400

    add geography filters to language downloader

--- census_data_downloader/language.py
+++ census_data_downloader/language.py
@@ -10,6 +10,7 @@ logger = logging.getLogger(__name__)
 @register_downloader
 class HouseholdLanguageDownloader(BaseDownloader):
     YEAR_LIST = (2017, 2016)
+    GEO_LIST = ("nationwide", "state", "county", "congressional_district", "place")
     PROCESSED_TABLE_NAME = 'householdlanguage'
     UNIVERSE = "households"
     RAW_TABLE_NAME = 'C16002'
