aaa7e073860fe5082802e97edf650dcb52bc5945
sandhya-k <sandhyak624@gmail.com>
2019-06-17 15:58:15 -0700

    add in updated internet subscription table

--- census_data_downloader/tables/internet.py
+++ census_data_downloader/tables/internet.py
@@ -10,18 +10,19 @@ class InternetDownloader(BaseTableConfig):
     YEAR_LIST = (2017,)
     PROCESSED_TABLE_NAME = "internet"
     UNIVERSE = "households"
-    RAW_TABLE_NAME = 'B28003'
+    RAW_TABLE_NAME = 'B28002'
     RAW_FIELD_CROSSWALK = collections.OrderedDict({
         '001E': 'universe',
-        '002E': 'total_with_computer',
-        '003E': 'dialup_internet',
+        '002E': 'total_with_internet_subscription',
+        '003E': 'dialup_internet_with_no_other_internet',
         '004E': 'broadband_internet',
-        '005E': 'computer_no_internet',
-        '006E': 'total_no_computer'
+        '005E': 'cellular_data',
+        '006E': 'cellular_data_with_no_other_internet',
+        '007E': 'total_with_internet_subscription_broadband',
+        '008E': 'with_internet_subscription_broadband_with_no_other_internet',
+        '009E': 'with_internet_satellite',
+        '010E': 'with_internet_satellite_with_no_other_internet',
+        '011E': 'with_internet_other_service_no_other_internet',
+        '012E': 'internet_access_without_a_subscription',
+        '013E': 'no_internet_access',  
     })
-
-    def _process_raw_data(self, *args, **kwargs):
-        df = super()._process_raw_data(*args, **kwargs)
-        df['total_with_internet'] = df['dialup_internet'] + df['broadband_internet']
-        df['total_no_internet'] = df['computer_no_internet'] + df['total_no_computer']
-        return df
