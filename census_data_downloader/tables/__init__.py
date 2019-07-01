8ea85918cfe45f67248669a114da11d1d57d7705
sandhya-k <sandhyak624@gmail.com>
2019-07-01 12:23:34 -0700

    actually nvm

--- census_data_downloader/tables/__init__.py
+++ census_data_downloader/tables/__init__.py
@@ -20,10 +20,7 @@ from .language import (
     LanguageShortFormDownloader,
     LanguageLongFormDownloader
 )
-from .latino import (
-    LatinoDownloader,
-    LatinoRaceDownloader
-)
+from .latino import LatinoDownloader
 from .medianage import MedianAgeDownloader
 from .mediangrossrent import MedianGrossRentDownloader
 from .medianhouseholdincome import (
