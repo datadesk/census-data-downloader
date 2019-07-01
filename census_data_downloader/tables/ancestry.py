e4056908e1c333c1b5c857db0a16ffb7e932abed
sandhya-k <sandhyak624@gmail.com>
2019-07-01 15:05:03 -0700

    ancestry table tweaks

--- census_data_downloader/tables/ancestry.py
+++ census_data_downloader/tables/ancestry.py
@@ -4,6 +4,7 @@ import collections
 from census_data_downloader.core.tables import BaseTableConfig
 from census_data_downloader.core.decorators import register
 
+
 @register
 class AncestryDownloader(BaseTableConfig):
     PROCESSED_TABLE_NAME = "singleancestry"
