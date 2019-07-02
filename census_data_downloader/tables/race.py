d654cb9bb5fc6e5a0b8e48728f6b74d8060edffc
sandhya-k <sandhyak624@gmail.com>
2019-07-02 13:45:46 -0700

    add in race tables

--- census_data_downloader/tables/race.py
+++ census_data_downloader/tables/race.py
@@ -11,15 +11,15 @@ class RaceDownloader(BaseTableConfig):
     UNIVERSE = "total population"
     RAW_TABLE_NAME = 'B03002'
     RAW_FIELD_CROSSWALK = collections.OrderedDict({
-        '001E': "universe",
-        '003E': "white_alone",
-        '004E': "black_alone",
-        '005E': "american_indian_and_alaska_native",
-        '006E': "asian_alone",
-        '007E': "native_hawaiian_and_pacific_islander",
-        '008E': "other_alone",
-        '009E': "two_or_more_races",
-        '012E': "latino_alone"
+        '001': "universe",
+        '003': "white_alone",
+        '004': "black_alone",
+        '005': "american_indian_and_alaska_native",
+        '006': "asian_alone",
+        '007': "native_hawaiian_and_pacific_islander",
+        '008': "other_alone",
+        '009': "two_or_more_races",
+        '012': "latino_alone"
     })
 
     def process(self, *args, **kwargs):
@@ -40,5 +40,5 @@ class AmericanIndianAlaskaNativeAloneOrInComboDownloader(BaseTableConfig):
     UNIVERSE = "people who are american indian or alaska native alone or in combination with one or more races"
     RAW_TABLE_NAME = 'B02010'
     RAW_FIELD_CROSSWALK = collections.OrderedDict({
-        "001E": "universe"
+        "001": "universe"
     })
