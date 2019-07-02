1f4b0291a8404a60cae00aaafd4e6bef2713b8cb
sandhya-k <sandhyak624@gmail.com>
2019-07-02 13:39:45 -0700

    cleaning

--- census_data_downloader/tables/yearstructurebuilt.py
+++ census_data_downloader/tables/yearstructurebuilt.py
@@ -15,41 +15,41 @@ class YearStructureBuiltDownloader(BaseTableConfig):
     def RAW_FIELD_CROSSWALK(self):
         if self._year > 2014:
             return collections.OrderedDict({
-                '001E': 'universe',
-                '002E': 'built_2014_or_later',
-                '003E': 'built_2010_to_2013',
-                '004E': 'built_2000_to_2009',
-                '005E': 'built_1990_to_1999',
-                '006E': 'built_1980_to_1989',
-                '007E': 'built_1970_to_1979',
-                '008E': 'built_1960_to_1969',
-                '009E': 'built_1950_to_1959',
-                '010E': 'built_1940_to_1949',
-                '011E': 'built_1929_or_earlier'
+                '001': 'universe',
+                '002': 'built_2014_or_later',
+                '003': 'built_2010_to_2013',
+                '004': 'built_2000_to_2009',
+                '005': 'built_1990_to_1999',
+                '006': 'built_1980_to_1989',
+                '007': 'built_1970_to_1979',
+                '008': 'built_1960_to_1969',
+                '009': 'built_1950_to_1959',
+                '010': 'built_1940_to_1949',
+                '011': 'built_1929_or_earlier'
             })
         elif self._year > 2011:
             return collections.OrderedDict({
-                '001E': 'universe',
-                '002E': 'built_2010_or_later',
-                '003E': 'built_2000_to_2009',
-                '004E': 'built_1990_to_1999',
-                '005E': 'built_1980_to_1989',
-                '006E': 'built_1970_to_1979',
-                '007E': 'built_1960_to_1969',
-                '008E': 'built_1950_to_1959',
-                '009E': 'built_1940_to_1949',
-                '010E': 'built_1939_or_earlier'
+                '001': 'universe',
+                '002': 'built_2010_or_later',
+                '003': 'built_2000_to_2009',
+                '004': 'built_1990_to_1999',
+                '005': 'built_1980_to_1989',
+                '006': 'built_1970_to_1979',
+                '007': 'built_1960_to_1969',
+                '008': 'built_1950_to_1959',
+                '009': 'built_1940_to_1949',
+                '010': 'built_1939_or_earlier'
             })
         else:
             return collections.OrderedDict({
-                '001E': 'universe',
-                '002E': 'built_2005_or_later',
-                '003E': 'built_2000_to_2004',
-                '004E': 'built_1990_to_1999',
-                '005E': 'built_1980_to_1989',
-                '006E': 'built_1970_to_1979',
-                '007E': 'built_1960_to_1969',
-                '008E': 'built_1950_to_1959',
-                '009E': 'built_1940_to_1949',
-                '010E': 'built_1939_or_earlier'
+                '001': 'universe',
+                '002': 'built_2005_or_later',
+                '003': 'built_2000_to_2004',
+                '004': 'built_1990_to_1999',
+                '005': 'built_1980_to_1989',
+                '006': 'built_1970_to_1979',
+                '007': 'built_1960_to_1969',
+                '008': 'built_1950_to_1959',
+                '009': 'built_1940_to_1949',
+                '010': 'built_1939_or_earlier'
             })
