e757bfdb8f2bb53dbe5b84db3a4bd795b6ba8f63
sandhya-k <sjkambhampati@gmail.com>
2019-07-19 10:46:29 -0700

    race moe aggregator updated

--- census_data_downloader/tables/race.py
+++ census_data_downloader/tables/race.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*
 import collections
+from pprint import pprint
+import census_data_aggregator
+from census_data_downloader.core import MOE_MAP
 from census_data_downloader.core.tables import BaseTableConfig
 from census_data_downloader.core.decorators import register
 
@@ -22,17 +25,46 @@ class RaceDownloader(BaseTableConfig):
         '012': "latino_alone"
     })
 
-    def process(self, *args, **kwargs):
-        df = super().process(*args, **kwargs)
+    def process(self, df):
         # Our custom race groups
-        df['asian_all'] = df['asian_alone'] + df['native_hawaiian_and_pacific_islander']
-        df['other_all'] = df[[
-            'american_indian_and_alaska_native',
-            'other_alone',
-            'two_or_more_races'
-        ]].sum(axis=1)
-        return df
+        def calculate_asians_moe(row):
+            pprint(row)
+            if row['asian_alone_moe'] in list (MOE_MAP.values()):
+                value = sum([row['asian_alone'], row['native_hawaiian_and_pacific_islander']])
+                moe = None
+            value, moe = census_data_aggregator.approximate_sum(
+                (row['native_hawaiian_and_pacific_islander'], row['native_hawaiian_and_pacific_islander_moe']),
+                (row['asian_alone'], row['asian_alone_moe']),
+            )
+            row['asians_all'] = value
+            row['asians_all_moe'] = moe
+            return row
 
+        df = df.apply(
+            calculate_asians_moe,
+            axis=1
+        )
+        def calculate_other_moe(row):
+            pprint(row)
+            if row['american_indian_and_alaska_native_moe'] in list (MOE_MAP.values()):
+                value = sum([row['american_indian_and_alaska_native'], row['other_alone'],row['two_or_more_races']])
+                moe = None
+            value, moe = census_data_aggregator.approximate_sum(
+                (row['american_indian_and_alaska_native'], row['american_indian_and_alaska_native_moe']),
+                (row['other_alone'], row['other_alone_moe']),
+                (row['two_or_more_races'], row['two_or_more_races_moe']),
+            )
+            row['other_all'] = value
+            row['other_all_moe'] = moe
+            return row
+
+        df = df.apply(
+            calculate_other_moe,
+            axis=1
+        )
+
+        # Pass it back
+        return df
 
 @register
 class AmericanIndianAlaskaNativeAloneOrInComboDownloader(BaseTableConfig):
