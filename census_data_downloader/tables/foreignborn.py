3a1bcc471a9981f71c7afbe9b7287e1232be0cf8
sandhya-k <sjkambhampati@gmail.com>
2019-07-22 17:18:24 -0700

    more tweaks to functions

--- census_data_downloader/tables/foreignborn.py
+++ census_data_downloader/tables/foreignborn.py
@@ -33,20 +33,21 @@ class CitizenDownloader(BaseTableConfig):
         "006": "not_us_citizen"
     })
 
-    def calculate_moe(self, df):
-        # our custom groups
-        if row['us_citizen_by_naturalization_moe'] in list(MOE_MAP.values()):
-            value = sum([row['us_citizen_by_naturalization'], row['us_citizen_born_abroad_american_parents'], row['us_citizen_born_puertorico_or_us_island_alone'], row['us_citizen_born_us']])
-            moe = None
-        value, moe = census_data_aggregator.approximate_sum(
-            (row['us_citizen_by_naturalization'], row['us_citizen_by_naturalization_moe']),
-            (row['us_citizen_born_abroad_american_parents'], row['us_citizen_born_abroad_american_parents_moe']),
-            (row['us_citizen_born_puertorico_or_us_island_alone'], row['us_citizen_born_puertorico_or_us_island_moe']),
-            (row['us_citizen_born_us'], row['us_citizen_born_us_moe']),
-        )
-        row['us_citizen_total'] = value
-        row['us_citizen_total_moe'] = moe
-        return row
+    def process(self, df):
+        def calculate_moe(row):
+            # our custom groups
+            if row['us_citizen_by_naturalization_moe'] in list(MOE_MAP.values()):
+                value = sum([row['us_citizen_by_naturalization'], row['us_citizen_born_abroad_american_parents'], row['us_citizen_born_puertorico_or_us_island_alone'], row['us_citizen_born_us']])
+                moe = None
+            value, moe = census_data_aggregator.approximate_sum(
+                (row['us_citizen_by_naturalization'], row['us_citizen_by_naturalization_moe']),
+                (row['us_citizen_born_abroad_american_parents'], row['us_citizen_born_abroad_american_parents_moe']),
+                (row['us_citizen_born_puertorico_or_us_island_alone'], row['us_citizen_born_puertorico_or_us_island_moe']),
+                (row['us_citizen_born_us'], row['us_citizen_born_us_moe']),
+            )
+            row['us_citizen_total'] = value
+            row['us_citizen_total_moe'] = moe
+            return row
 
         df = df.apply(
             calculate_moe,
