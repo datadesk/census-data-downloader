aa77557606d22e0dada7239ccd91672bf364ec99
Sandhya Kambhampati <sjkambhampati@gmail.com>
2019-09-27 15:47:55 -0700

    v0.0.16

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='census-data-downloader',
-    version='0.0.15',
+    version='0.0.16',
     description="Download U.S. census data and reformat it for humans",
     author='Los Angeles Times Data Desk',
     author_email='datadesk@latimes.com',
