f067b0776eb30c8b376480b390a6b002495ae08f
Sandhya Kambhampati <sjkambhampati@gmail.com>
2019-12-18 11:49:59 -0800

    2018 5 year data get excited

--- census_data_downloader/core/geotypes.py
+++ census_data_downloader/core/geotypes.py
@@ -21,6 +21,7 @@ class BaseGeoTypeDownloader(object):
     Expects a TableConfig instance and year as input.
     """
     YEAR_LIST = [
+        2018,
         2017,
         2016,
         2015,
@@ -357,6 +358,7 @@ class CnectasDownloader(BaseGeoTypeDownloader):
     Download raw data at the combined New England city and town level.
     """
     YEAR_LIST = [
+        2018,
         2017,
         2016,
         2015,
@@ -395,6 +397,7 @@ class ZctasDownloader(BaseGeoTypeDownloader):
     Download raw data at the zipcode-tabulation-area level.
     """
     YEAR_LIST = [
+        2018,
         2017,
         2016,
         2015,
