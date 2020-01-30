fca0677fd5d199fc447935f508172dd6a470f2b0
Sandhya Kambhampati <sjkambhampati@gmail.com>
2020-01-29 18:19:49 -0800

    fixes

--- census_data_downloader/tables/__init__.py
+++ census_data_downloader/tables/__init__.py
@@ -98,8 +98,8 @@ from .yearstructurebuilt import (
     AgeHouseholderByYearBuiltDownloader
 )
 from .voterage import VoterDownloader
-from .voterbyeducation import VoterEducationDownloader
-from .voterbyincome import importVoterIncomeDownloader
+from .votereducation import VoterEducationDownloader
+from .voterincome import VoterIncomeDownloader
 
 __all__ = [
     TABLE_LIST,
