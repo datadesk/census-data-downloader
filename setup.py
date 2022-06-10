d164140838a0730270d3cc64a8fd9d930b95ed37
Sandhya Kambhampati <sjkambhampati@gmail.com>
2022-06-10 09:13:42 -0700

    v0.0.35

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     
 setup(
     name='census-data-downloader',
-    version='0.0.34',
+    version='0.0.35',
     description="Download U.S. census data and reformat it for humans",
     long_description=long_description,
     long_description_content_type='text/markdown',
